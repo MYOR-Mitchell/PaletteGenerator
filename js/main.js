e20817a0b8bbd17c43ee659d039548832539ac83
MYOR-Mitchell <Martin.Mitchell@MyYahoo.com>
2025-03-06 14:27:40 -0500

    Button functions
    Random button functionality added.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+fetch("https://palettegeneratorapi.onrender.com/api/palette/generate");
+
 let myList = [
     "--base-clr: ",
     "--section-clr: ",
@@ -23,24 +25,52 @@ document.getElementById("copyBtn").addEventListener("click", function () {
     alert("Copied to clipboard");
 });
 
-document.getElementById("generateBtn").addEventListener("click", function () {
+function applyPalette() {
     const colorInputs = document.querySelectorAll("input[type='text']");
 
     colorInputs.forEach(input => {
-        let colorVarUpdate = input.getAttribute("color-var"); 
-        let colorValue = input.value; 
+        let colorVarUpdate = input.getAttribute("color-var");
+        let colorValue = input.value;
         document.documentElement.style.setProperty(colorVarUpdate, colorValue);
     });
-});
+}
+
+document.getElementById("generateBtn").addEventListener("click", applyPalette); 
 
 document.querySelectorAll("input[type='color']").forEach(colorInput => {
     colorInput.addEventListener("input", function () {
-        this.nextElementSibling.value = this.value; 
+        this.nextElementSibling.value = this.value;
     });
 });
 
 document.querySelectorAll("input[type='text']").forEach(colorInput => {
     colorInput.addEventListener("input", function () {
-        this.previousElementSibling.value = this.value; 
+        this.previousElementSibling.value = this.value;
     });
+});
+
+
+document.getElementById("randomBtn").addEventListener("click", function () {
+    fetch("https://palettegeneratorapi.onrender.com/api/palette/generate")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("base").value = data.baseClr;
+            document.getElementById("section").value = data.sectionClr;
+            document.getElementById("text").value = data.textClr;
+            document.getElementById("secondaryText").value = data.secondaryTextClr;
+            document.getElementById("accent").value = data.accentClr;
+            document.getElementById("line").value = data.lineClr;
+            document.getElementById("hover").value = data.hoverClr;
+            document.getElementById("shadow").value = data.shadowClr;
+
+            document.getElementById("base").previousElementSibling.value = data.baseClr;
+            document.getElementById("section").previousElementSibling.value = data.sectionClr;
+            document.getElementById("text").previousElementSibling.value = data.textClr;
+            document.getElementById("secondaryText").previousElementSibling.value = data.secondaryTextClr;
+            document.getElementById("accent").previousElementSibling.value = data.accentClr;
+            document.getElementById("line").previousElementSibling.value = data.lineClr;
+            document.getElementById("hover").previousElementSibling.value = data.hoverClr;
+            document.getElementById("shadow").previousElementSibling.value = data.shadowClr;
+        })
+        .then(() => applyPalette())
 });
